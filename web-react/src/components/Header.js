2d8788e2e9db9dbffdaeaabfb2628f8c85884b21
Shah0912 <adi.shah0912@gmail.com>
2020-10-22 17:42:51 +0530

    Added header component

---
+++ web-react/src/components/Header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Header.css";
+import StreetviewIcon from '@material-ui/icons/Streetview';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add'; // For posting
+import { Avatar, IconButton } from "@material-ui/core";
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+const Header = () => {
+  return (
+    <div className="header">
+
+      <div className="headerLeft">
+        <img alt ="logo" src="" />
+        <div className="headerInput">
+          <SearchIcon />
+          <input placeholder="Search" type = "text" />
+        </div>
+        
+      </div>
+
+      
+      <div className="headerCenter">
+
+        <div className="headerOption headerOption--active">
+          <HomeIcon fontSize ="large"/>
+        </div>
+
+        <div className="headerOption">
+          <StreetviewIcon  fontSize ="large"/>
+        </div>
+
+        {/* <div className="homeOptions">
+          <IconButton>
+          <SupervisedUserCircleIcon  fontSize ="large"/>
+          </IconButton>
+          
+        </div> */}
+
+      </div>
+
+      <div className="headerRight">
+        
+        <div className="headerInfo">
+          <IconButton>
+            <Avatar fontSize="small"/>
+          </IconButton>
+          <h4>Aditya Shah</h4>
+        </div>
+
+        <IconButton className = "iconLeft">
+          <AddIcon />
+        </IconButton>
+        <IconButton className = "iconLeft">
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton className = "iconLeft">
+          <ArrowDropDownIcon />
+        </IconButton>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Header;
