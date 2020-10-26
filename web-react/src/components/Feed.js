a5ae814bbfcada8e7dbc5a45a60bdc4155e5f37d
Shah0912 <adi.shah0912@gmail.com>
2020-10-26 11:23:39 +0530

    Added posts

--- web-react/src/components/Feed.js
+++ web-react/src/components/Feed.js
@@ -1,15 +1,43 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react'
+import React, {useState} from 'react'
 import StoryReel from './StoryReel'
 import Sidebar from './Sidebar'
+import Post from './Post'
 
 import "./Feed.css";
 
 function Feed() {
+
+    const [posts, setPosts] = useState([
+        {
+            username: 'Aditya Shah', 
+            profileImg: 'https://www.news4jax.com/resizer/b89RYEm5oAgzxJxWIGoyLJ9lZu8=/960x960/smart/filters:format(jpeg):strip_exif(true):strip_icc(true):no_upscale(true):quality(65)/cloudfront-us-east-1.images.arcpublishing.com/gmg/X462YQ4HIJEGHHX2I3LXRV4G7A.jpg',
+            imgSrc : 'https://i.ytimg.com/vi/ck4RGeoHFko/maxresdefault.jpg',
+            caption: 'Yesssssss'
+        },
+        {
+            username: 'Sarang Shekokar', 
+            profileImg: 'https://images.alphacoders.com/711/thumb-350-711581.jpg',
+            imgSrc : 'https://www.htxt.co.za/wp-content/uploads/2019/02/Kurzgesagt-Loneliness.png',
+            caption: 'A good caption..'
+        }
+    ]);
+
+
     return (
         <div className="feed">
+            
             <StoryReel />
-            {/* <Sidebar /> */}
+            {/* <Post username="Aditya Shah" profileImg = "https://www.news4jax.com/resizer/b89RYEm5oAgzxJxWIGoyLJ9lZu8=/960x960/smart/filters:format(jpeg):strip_exif(true):strip_icc(true):no_upscale(true):quality(65)/cloudfront-us-east-1.images.arcpublishing.com/gmg/X462YQ4HIJEGHHX2I3LXRV4G7A.jpg" imgSrc = "https://i.ytimg.com/vi/ck4RGeoHFko/maxresdefault.jpg" caption = "Yesssssss"/>
+            <Post username="Sarang Shekokar" profileImg = "https://images.alphacoders.com/711/thumb-350-711581.jpg" imgSrc = "https://www.htxt.co.za/wp-content/uploads/2019/02/Kurzgesagt-Loneliness.png" caption = "ABC"/>
+            <Post username="Satish Gangula" profileImg = "https://i.kinja-img.com/gawker-media/image/upload/t_original/ijsi5fzb1nbkbhxa2gc1.png" imgSrc = "https://mir-s3-cdn-cf.behance.net/projects/404/9b3f9078805941.Y3JvcCwxNDg4LDExNjMsMCww.png" caption = "good caption..."/> */}
+            {/* <Post /> */}
+            {console.log(posts)}
+            {
+                posts.map((post)=> (
+                    <Post username={post.username} caption={post.caption} imgSrc={post.imgSrc} profileImg={post.profileImg}/>
+                ))
+            }
         </div>
     )
 }
--- web-react/src/components/Post.js
+++ web-react/src/components/Post.js
@@ -1,9 +1,28 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
+
+
 import "./Post.css"
 
-function Post() {
+function Post({username, profileImg, imgSrc, caption}) {
     return (
         <div className="post">
+
+            <div className="postHeader">
+                <Avatar 
+                    className="postAvatar"
+                    alt = {username}
+                    src = {profileImg}
+                    // alt={username}
+                    // alt="AdityaShah"
+                    // src="https://images.alphacoders.com/711/thumb-350-711581.jpg"
+                />
+                <h3>{username}</h3>
+            </div>
+
+            <img className="postImage" src={imgSrc} />
+
+            <h4 className="postText"><strong>{username} : </strong>{caption}</h4>
             
         </div>
     )
