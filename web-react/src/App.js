d6e28c47726b623d54e3aed7400e992a50ede732
Sarang Shekokar <shekokarsarang@gmail.com>
2020-11-09 23:52:00 +0530

    added 2 mutations

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -8,6 +8,7 @@ import Map from './components/social_street_view/Map'
 import Upload from './components/data_aquisition/Upload'
 import Ipost from './components/individual_post/Ipost'
 import Sentiment from './components/sentiment/Sentiment'
+import Test from './components/test'
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/street_view" component={Map} />
         <Route path="/profile" component={Profile} />
         <Route path="/sentiment" component={Sentiment} />
+        <Route path="/test" component={Test} />
         {/* <Upload /> */}
         {/* <Ipost /> */}
       </Router>
